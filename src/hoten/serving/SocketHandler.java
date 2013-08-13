49c20faf363cfcb21677dd2ab53a474236899abb
Hoten <cjamcl@gmail.com>
2013-08-12 22:22:22 -0500

    KISS: removed DATA_SIZE
    let's just keep it simple. message length is encoded in an integer, and
    message type in a short

--- src/hoten/serving/SocketHandler.java
+++ src/hoten/serving/SocketHandler.java
@@ -15,39 +15,17 @@ import java.net.Socket;
  */
 public abstract class SocketHandler {
 
-    //DATA_SIZE allows for customization in the bitsize of message properties (read: buffer and type)
-    public enum DATA_SIZE {
-
-        BYTE(1), SHORT(2), INT(4);
-        final int length;
-
-        DATA_SIZE(int length) {
-            this.length = length;
-        }
-    }
     final private Socket socket;
     final private DataInputStream in;
     final private DataOutputStream out;
-    final private DATA_SIZE IN_MSG_SIZE;
-    final private DATA_SIZE IN_MSG_TYPE;
-    final private DATA_SIZE OUT_MSG_SIZE;
-    final private DATA_SIZE OUT_MSG_TYPE;
     private boolean isOpen;
 
-    public SocketHandler(Socket socket, DATA_SIZE IN_MSG_SIZE, DATA_SIZE IN_MSG_TYPE, DATA_SIZE OUT_MSG_SIZE, DATA_SIZE OUT_MSG_TYPE) throws IOException {
+    public SocketHandler(Socket socket) throws IOException {
         this.socket = socket;
-        this.IN_MSG_SIZE = IN_MSG_SIZE;
-        this.IN_MSG_TYPE = IN_MSG_TYPE;
-        this.OUT_MSG_SIZE = OUT_MSG_SIZE;
-        this.OUT_MSG_TYPE = OUT_MSG_TYPE;
         in = new DataInputStream(socket.getInputStream());
         out = new DataOutputStream(socket.getOutputStream());
         isOpen = true;
         startReadingThread();
-    }
-
-    public SocketHandler(Socket socket) throws IOException {
-        this(socket, DATA_SIZE.INT, DATA_SIZE.BYTE, DATA_SIZE.INT, DATA_SIZE.BYTE);
     }
 
     protected abstract void handleData(ByteArray reader) throws IOException;
@@ -63,20 +41,8 @@ public abstract class SocketHandler {
             int type = message.getType();
             try {
                 synchronized (out) {
-                    if (OUT_MSG_SIZE == DATA_SIZE.INT) {
-                        out.writeInt(messageLength);
-                    } else if (OUT_MSG_SIZE == DATA_SIZE.SHORT) {
-                        out.writeShort(messageLength);
-                    } else if (OUT_MSG_SIZE == DATA_SIZE.BYTE) {
-                        out.writeByte(messageLength);
-                    }
-                    if (OUT_MSG_TYPE == DATA_SIZE.INT) {
-                        out.writeInt(type);
-                    } else if (OUT_MSG_TYPE == DATA_SIZE.SHORT) {
-                        out.writeShort(type);
-                    } else if (OUT_MSG_TYPE == DATA_SIZE.BYTE) {
-                        out.writeByte(type);
-                    }
+                    out.writeInt(messageLength);
+                    out.writeShort(type);
                     out.write(b);
                 }
             } catch (IOException ex) {
@@ -100,24 +66,9 @@ public abstract class SocketHandler {
 
     private void handleData() {
         try {
-            int buffer = 0, type = 0;
-
             //read the buffer and message type
-            if (IN_MSG_SIZE == DATA_SIZE.INT) {
-                buffer = in.readInt();
-            } else if (IN_MSG_SIZE == DATA_SIZE.SHORT) {
-                buffer = in.readShort();
-            } else if (IN_MSG_SIZE == DATA_SIZE.BYTE) {
-                buffer = in.readByte();
-            }
-
-            if (IN_MSG_TYPE == DATA_SIZE.INT) {
-                type = in.readInt();
-            } else if (IN_MSG_TYPE == DATA_SIZE.SHORT) {
-                type = in.readShort();
-            } else if (IN_MSG_TYPE == DATA_SIZE.BYTE) {
-                type = in.readByte();
-            }
+            int buffer = in.readInt();
+            int type = in.readShort();
 
             //load the data into a byte array
             //DataInputStream must read in chunks until the entire message is read
@@ -139,11 +90,6 @@ public abstract class SocketHandler {
                 reader.setType(type);
                 reader.rewind();
                 handleData(reader);
-            }
-
-            //recursively call if there is another message to read
-            if (in.available() > IN_MSG_SIZE.length + IN_MSG_TYPE.length) {
-                handleData();
             }
         } catch (IOException ex) {
             close();
