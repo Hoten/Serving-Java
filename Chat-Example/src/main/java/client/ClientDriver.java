f2fa234c168427052ad8b1d962ba0c3e0720f005
Hoten <cjamcl@gmail.com>
2014-11-03 00:39:49 -0600

    System.out.println -> chat.announce

--- Chat-Example/src/main/java/client/ClientDriver.java
+++ Chat-Example/src/main/java/client/ClientDriver.java
@@ -6,8 +6,8 @@ import java.net.Socket;
 public class ClientDriver {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Connecting to server...");
         Chat chat = new Chat();
+        chat.announce("Connecting to server...");
         ConnectionToChatServerHandler serverConnection = new ConnectionToChatServerHandler(chat, new Socket("localhost", 1234));
         serverConnection.start();
     }
